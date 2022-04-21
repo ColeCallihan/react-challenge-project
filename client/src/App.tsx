62c43b2fe6a7fcc97a000a102c0ca4373fb05c52
ColeCallihan <callicole5@hotmail.com>
2022-04-21 11:34:00 -0600

    Still playing around with Axios

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import axios from 'axios';
 import './App.css';
 import { FinalTable } from './Components/FinalTable';
@@ -11,28 +11,45 @@ import { FinalTable } from './Components/FinalTable';
 //Text search for name, city, or product
 //Make it presentable
 
+//Access to XMLHttpRequest at 'http://localhost:3001/companies' from origin 'http://localhost:3000' has been blocked by CORS policy: No 'Access-Control-Allow-Origin' header is present on the requested resource.
+//Final error to figure out
+
+//AsyncAwait
+
+const { useEffect, useState } = React;
+
 function App() {
 
   const [quote, setQuote] = useState('')
+  const [userData, setUserData] = useState('');
+
   //Using Axios to dynamically grab data
-  const getQuote = () => {
+  const getData = () => {
   axios.get('http://localhost:3001/companies')
-    .then(response => {
+    .then(({data}) => {
       //Logging the response
-      console.log(response);
+      console.log(data);
       //Using a function to send it to variable "quote"
-      setQuote(response.data);
+      return JSON.stringify(data);
     })
     .catch(err => {
-      console.log(err);
+      console.error(err);
     })
   }
+
+  useEffect(() => {
+    //Mimics component did mount 
+    getData().then(userData => {
+      setUserData(userData || 'No user data');
+    })
+  }, []);
 
   return (
     <div className="App">
       <FinalTable />
-      <button onClick={getQuote}>Grab Data</button>
-      { quote && <p>{quote}</p>}
+      <p>
+        {userData}
+      </p>
     </div>
   );
 }
